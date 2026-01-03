c3ceadd8d4b5d52cb7761de6c3902354790daaa7
ajartijaouad <ajartijaouad14@gmail.com>
2026-01-03 14:45:06 +0100

    ajouter la fpnctionnnalité de  logout de user

--- view/login.php
+++ view/login.php
@@ -14,6 +14,7 @@ if (isset($_POST['login'])) {
     if ($user && password_verify($password, $user['password'])) {
         $_SESSION['user_id'] = $user['user_id'];
         $_SESSION['username'] = $user['username'];
+        $_SESSION['role'] = $user['role'];
         header('Location: dashboard.php');
         exit;
     } else {
--- view/register.php
+++ view/register.php
@@ -32,7 +32,7 @@ if (isset($_POST['register'])) {
     <title>Inscription</title>
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
-
+   
 <body class="bg-gray-100 flex items-center justify-center min-h-screen">
 
     <div class="bg-[#0C172C] w-full max-w-md p-8 rounded-lg shadow-lg">
