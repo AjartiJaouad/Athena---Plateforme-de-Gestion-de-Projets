95a07a948d46b74160ce8fb7bd919451614547df
ajartijaouad <ajartijaouad14@gmail.com>
2026-01-07 14:23:51 +0100

    creation de la clas adminservice avec une  recuperation de tout les utlisateurs

---
+++ services/AdminService.php
@@ -0,0 +1,39 @@
+<?php 
+
+
+require_once __DIR__ . '/../entites/User.php';
+
+class AdminService {
+
+
+    private PDO $db ;
+    public function __construct(PDO $db)
+    {
+       $this->db = $db ;
+    }
+
+ public function getAllUsers(): array
+    {
+        $stmt = $this->db->prepare("SELECT * FROM user ORDER BY user_id ASC");
+        $stmt->execute();
+        $users = [];
+        while ($data = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            $users[] = new User(
+                $data['user_id'],
+                $data['username'],
+                $data['email'],
+                $data['password'],
+                $data['role']
+            );
+        }
+        return $users;
+    }
+
+
+
+
+
+
+}
+
+?>
